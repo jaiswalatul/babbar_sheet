82ff3b83ce48cf07652e0551bead6ffefbbb1a86
atul jaiswal <jaiswalatul018@gmai.com>
2024-03-03 20:51:17 +0530

    new

---
+++ stack_&queue/reverse_queue_using_recursion.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//function Template for C++
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    
+    void rev1(queue<int>&q1){
+         if(q1.size()==0){
+            return;
+        }
+        int x=q1.front();
+        q1.pop();
+        rev1(q1);
+        q1.push(x);
+    }
+    
+    
+    
+    queue<int> rev(queue<int> q)
+    {
+        // add code here.
+        rev1(q);
+        return q;
+       
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    Solution ob;
+    queue<int> a=ob.rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
