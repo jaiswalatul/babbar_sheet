6c624ff01a9ed0c9f48ce101afb096b8bedbde7a
atul jaiswal <jaiswalatul018@gmai.com>
2024-03-05 23:35:27 +0530

    new

--- stack_&queue/next_greater_elment.cpp
+++ stack_&queue/next_greater_elment.cpp
@@ -114,3 +114,72 @@ int main()
 	return 0;
 }
 // } Driver Code Ends
+
+//////////  Using stack
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    
+    public:
+    vector<int> help_classmate(vector<int> arr, int n) 
+    { 
+        // Your code goes herefor
+        stack<int>s;
+        vector<int>v;
+        v.push_back(-1);
+        s.push(arr[n-1]);
+        for(int i=n-2 ;i>=0;i--){
+            while(s.empty()==0  &&s.top()>=arr[i]){
+                s.pop();
+            }
+            if(s.size()==0){
+                v.push_back(-1);
+            }
+            else{
+                v.push_back(s.top());
+            }
+            s.push(arr[i]);
+        }
+        reverse(v.begin(),v.end());
+        return v;
+        
+    } 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<int> array(n);
+		for (int i = 0; i < n; ++i)
+		{
+			cin>>array[i];
+		}
+		Solution obj;
+		vector<int> result = obj.help_classmate(array,n);
+		for (int i = 0; i < n; ++i)
+		{
+			cout<<result[i]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0; 
+}
+
+
+// } Driver Code Ends
